f8e5ead23b749a9d95df98b6fea7ef08aeae816c
Andre Du Plessis <andre.du.plessis@adpc-llc.com>
2024-09-12 08:23:53 +0200

    Update vue.config.ts

--- benchmark-set/vue.config.ts
+++ benchmark-set/vue.config.ts
@@ -1,6 +1,10 @@
+/** @format */
+
 import { defineConfig } from "@vue/cli-service";
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath:
-    process.env.NODE_ENV === "production" ? "/<your-repo-name>/" : "/",
+    process.env.NODE_ENV === "production"
+      ? "/github.com/ADPC-LLC/Best-Lang/"
+      : "/",
 });
